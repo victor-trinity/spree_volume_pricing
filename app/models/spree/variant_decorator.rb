310b34670bfc316c5d669a300c521cc2b24a8a1f
Victor H. Salazar <victor@ninjadevs.com>
2017-12-13 00:46:28 -0500

    Rename join table

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,5 +1,6 @@
 Spree::Variant.class_eval do
-  has_and_belongs_to_many :volume_price_models
+
+  has_and_belongs_to_many :volume_price_models, join_table: 's_variants_volume_price_models'
   has_many :volume_prices, -> { order(position: :asc) }, dependent: :destroy
   has_many :model_volume_prices,-> { order(position: :asc) }, class_name: "Spree::VolumePrice", through: :volume_price_models, source: :volume_prices
   accepts_nested_attributes_for :volume_prices, allow_destroy: true,
